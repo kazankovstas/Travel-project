747021e219ed858d6d6e7eec80362971cca254c4
Stanislav <kazankovstas.jb@gmail.com>
2022-06-03 22:19:17 +0300

    feat: main-nav

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 {
   $(function () {
-    $(".popular__list, .feedback__list").slick({
+    $(".popular__list").slick({
       dots: true,
       arrows: true,
       infinite: true,
@@ -11,5 +11,33 @@
       prevArrow: $(".prev"),
       nextArrow: $(".next"),
     });
+  });
+}
+
+{
+  $(function () {
+    $(".feedback__list").slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      prevArrow: $(".f-prev"),
+      nextArrow: $(".f-next"),
+    });
+  });
+}
+
+{
+  const openMenu = document.querySelector(".navigation__toggle");
+  const menu = document.querySelector(".navigation__wrapper");
+
+  openMenu.addEventListener("click", function () {
+    openMenu.classList.toggle("navigation__toggle_active");
+    openMenu.classList.contains("navigation__toggle_active")
+      ? menu.classList.add("navigation__wrapper_opened")
+      : menu.classList.remove("navigation__wrapper_opened");
   });
 }
